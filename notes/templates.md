b045829b0c8acbc388cc61bcb867f929445c901b
Eric Ekholm <eric.ekholm@gmail.com>
2022-07-15 15:48:04 -0400

    data into templates

--- notes/templates.md
+++ notes/templates.md
@@ -4,5 +4,16 @@
  - file extension doesn't really matter when reading into go (for text templates)
  - a pointer to a template is a "container" into which all templates get parsed and are held. Todd's words -- obv not technical
  - see a very basic example of parsing and printing a text template in `templates/02/...`
+ - sort of misc, but something like (filename ...string) as a function arg is a *variadic parameter*, meaning it can take 1 or more arguments
 
- *resume at 5 mins into video 8 understanding package text/template...*
+ - a pointer to a template (e.g. *template.Template) will have a bunch of methods available to it, e.g. Execute()
+
+ - see `templates/02/main.go` for parsing multipel templates via ParseGlob
+
+ ## Passing Data into Templates
+ - *see `templates/03/...` for putting data into templates
+ - {{.}} in a template represents the current piece of data
+ - note that you can only pass in one piece of data into a template, but this piece of data can be a struct or something that has lots of things in it
+ - see `tpl2.html` for how to use a variable in a template. Not sure why you'd want this, though?
+
+ **resume at video 11 - composite data structures in templates**
--- templates/02/main.go
+++ templates/02/main.go
@@ -6,14 +6,64 @@ import (
 	"os"
 )
 
+//executing a single template
+// func main() {
+// 	tpl, err := template.ParseFiles("templates/02/tpl.html")
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+
+// 	err = tpl.Execute(os.Stdout, nil)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// }
+
+//multiple templates
+// func main() {
+// 	tpl, err := template.ParseFiles("templates/02/tpl.html", "templates/02/tpl2.html")
+
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+
+// 	//execute template 1
+// 	err = tpl.ExecuteTemplate(os.Stdout, "tpl.html", nil)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+
+// 	//execute template 2
+// 	err = tpl.ExecuteTemplate(os.Stdout, "tpl2.html", nil)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+
+// }
+
+//initialize the program by parsing all of the templates
+//ensures things just get parsed once
+//will make the program slightly more performant
+var tpl *template.Template
+
+func init() {
+
+	//Must will panic if there's an error reading the template
+	tpl = template.Must(template.ParseGlob("templates/02/*.html"))
+}
+
+//a better way to do multiple files via ParseGlob
 func main() {
-	tpl, err := template.ParseFiles("templates/02/tpl.html")
+	//execute template 1
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.html", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	err = tpl.Execute(os.Stdout, nil)
+	//execute template 2
+	err = tpl.ExecuteTemplate(os.Stdout, "tpl2.html", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 }
