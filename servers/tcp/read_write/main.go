932c007959fc5c45a3ee84b473da73cfbc8b8854
Eric Ekholm <eric.ekholm@gmail.com>
2022-08-11 06:42:03 -0400

    more tcp server stuff

--- servers/tcp/read_write/main.go
+++ servers/tcp/read_write/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 func main() {
@@ -19,7 +22,35 @@ func main() {
 		if err != nil {
 			log.Panic(err)
 		}
+		go handle(conn)
 	}
 
-	//RESUME AT 5:30 IN 22. TCP SERVER READ FROM CONNECTION VID
 }
+
+//handler function to use in our goroutine above
+func handle(conn net.Conn) {
+
+	//set a deadline on the connection
+	//this will close the connection after 10 seconds
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Println("Conn Timeout!")
+	}
+
+	//create a new scanner using the connection
+	scanner := bufio.NewScanner(conn)
+
+	//for all of the lines in whatever we're reading, get the text (which will break with line breaks, i think), and return it.
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+
+	fmt.Println("Now we done")
+
+	defer conn.Close()
+}
+
+//to see this work, go to the terminal and connect to telnet localhost 8080
+//then start typing in some messages
